c81610aaf815902b1b9c029970636e274c287eee
ICE <ice@ICEdeMacBook-Air.local>
2024-02-25 13:20:12 +0800

    成功建立vue網頁,並將資料存入資料庫

--- plain_vue/src/components/IceForm.vue
+++ plain_vue/src/components/IceForm.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
     <form @submit.prevent="submitForm">
-      <label for="a">a:</label>
-      <input type="text" id="a" v-model="formData.a" required>
+      <label for="name">name:</label>
+      <input type="text" id="name" v-model="formData.name" autocomplete="name" required>
 
-      <label for="b">b:</label>
-      <input type="text" id="b" v-model="formData.b" required>
+      <label for="age">age:</label>
+      <input type="text" id="age" v-model="formData.age" autocomplete="age" required>
+
+      <label for="gender">gender:</label>
+      <input type="text" id="gender" v-model="formData.gender" autocomplete="gender" required>
+
+      <label for="detail">detail:</label>
+      <input type="text" id="detail" v-model="formData.detail" autocomplete="detail" required>
 
       <button type="submit">Submit</button>
     </form>
@@ -19,15 +25,17 @@ export default {
   data() {
     return {
       formData: {
-        a: '',
-        b: '',
+        name: '',
+        age: '',
+        gender: '',
+        detail: '',
       },
     };
   },
   methods: {
     submitForm() {
       // 使用 Axios 發送 POST 請求到 Flask 服務器
-      axios.post('http://127.0.0.1:5001/a', this.formData)
+      axios.post('http://127.0.0.1:5001/add_info', this.formData)
         .then(response => {
           // 在這裡處理服務器的回應
           console.log(response.data);
--- run.py
+++ run.py
@@ -1,15 +1,52 @@
-from flask import Flask,request
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from pymongo import MongoClient
+import os
 
-app=Flask(__name__)
+MONGO_HOST = os.environ.get('MONGO_HOST', '127.0.0.1:27017')
+
+app = Flask(__name__)
+CORS(app)
+client = MongoClient(MONGO_HOST)
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def status():
+    return 'vue_api ok'
+
+
+@app.route('/add_info', methods=['POST'])
+def add_info_json():
+    col = client['vue']['info']
+
+    data = request.get_json()
+
+    col.insert_one(data)
+
+    name = data.get('name')
+    age = data.get('age')
+    gender = data.get('gender')
+    detail = data.get('detail')
+
+    # Add CORS headers
+    response = jsonify({'message': 'Success'})
+    response.headers.add('Access-Control-Allow-Origin',
+                         'http://localhost:8081')
+    response.headers.add('Access-Control-Allow-Methods', 'POST')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+
+    # 檢查是否有空值
+    if not all([name, age, gender, detail]):
+        return jsonify({'error': f'All fields must be provided.\n{request.form}'}), 400
+
+    # 在這裡可以進一步處理接收到的數據
+    return jsonify({'message': 'Information added successfully.'})
+
 
 @app.route('/a', methods=['*'])
 def add():
     data = request.form.get('data')
 
-if __name__ == '__main__':
-    app.run(port=5001,host='0.0.0.0')
 
+if __name__ == '__main__':
+    app.run(port=5001, host='0.0.0.0')
